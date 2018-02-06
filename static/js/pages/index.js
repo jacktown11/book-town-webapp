69ff93142a9163665722389e82ac2c5ae37b6dd0
jacktown11 <jikai_tang@163.com>
2018-02-06 08:37:37 +0800

    合并阅读器功能前

--- static/js/pages/index.js
+++ static/js/pages/index.js
@@ -1,7 +1,11 @@
 $.get('/ajax/index', function(data){
+    var screenWidth = $(window).width();
+    screenWidth = screenWidth < 300 ? 300 : screenWidth;
     var app = new Vue({
-        el: '#app',
+        el: '#root',
         data: {
+            screenWidth: screenWidth,
+            transPos: 0,
             top: data.items[0].data.data,
             hot: data.items[1].data.data,
             recommend: data.items[2].data.data,
@@ -9,6 +13,16 @@ $.get('/ajax/index', function(data){
             female: data.items[4].data.data,
             free: data.items[5].data.data,            
             topic: data.items[6].data.data            
+        },
+        methods: {
+            toggleMainSection: function(swipeTo){
+                // toggle between booktown and bookshelf
+                if(swipeTo === 'bookshelf'){
+                    this.transPos = -screenWidth;
+                }else{
+                    this.transPos = 0;
+                }
+            }
         }
     });
 }, 'json');
